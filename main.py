42dc008e8c06cee8f520d8c38e0d4d51978ce641
tshirtman <gabriel.pettier@gmail.com>
2013-10-25 23:43:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.properties import NumericProperty
+
+# needed for kv
+from kivy.garden import magnet
+#from kivy.garden.magnet import transitions
+
+transitions = (
+    'out_sine', 'out_quint', 'out_quart', 'out_quad', 'out_expo',
+    'out_cubic', 'out_circ', 'out_bounce', 'out_back', 'linear',
+    'in_sine', 'in_quint', 'in_quart', 'in_quad', 'in_out_sine',
+    'in_out_quint', 'in_out_quart', 'in_out_quad', 'in_out_expo',
+    'in_out_cubic', 'in_out_circ', 'in_out_bounce', 'in_out_back',
+    'in_expo', 'in_cubic', 'in_circ', 'in_bounce', 'in_back',)
+
+
+class PresApp(App):
+    page = NumericProperty(0)
+
+    def build(self):
+        Window.bind(on_keyboard=self.manage_key)
+        return super(PresApp, self).build()
+
+    def manage_key(self, window, key, *args):
+
+        if key in (273, 276):
+            self.page -= 1
+
+        elif key in (274, 275):
+            self.page += 1
+
+        self.page %= len(self.root.ids.gl.children)
+
+        print key
+
+    def on_page(self, *args):
+        print self.page
+
+if __name__ == '__main__':
+    PresApp().run()
